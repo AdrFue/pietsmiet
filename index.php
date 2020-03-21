843536b8c1c110a6d2307018985ed9749ce3c28c
Adrian F <af.bleistift@gmail.com>
2020-03-22 00:01:03 +0100

    Tabelle mit DB-Daten angepasst 4

--- index.php
+++ index.php
@@ -36,9 +36,9 @@ $rows = $statement->fetchAll();
         ?>
             <tr>
                 <td><?php  echo $row['titel'];?></td>
-                <td><?php  echo $row['laenge']/60 . ':' . $row['laenge']%60;?></td>
+                <td><?php  echo floor($row['laenge']/60) . ':' . $row['laenge']%60;?></td>
                 <td><?php  echo $row['datum'];?></td>
-                <td><?php  echo $row['link'];?></td>
+                <td><a href="<?php  echo $row['link'];?>"></a></td>
             </tr>
         <?php
         }
